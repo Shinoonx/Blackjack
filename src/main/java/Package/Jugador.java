2e04be72488207b65a619ef8b98dc8afd2a6c297
Ignacio <ivaldebenito52@gmail.com>
2023-05-25 19:50:49 -0400

    Initial commit

---
+++ src/main/java/Package/Jugador.java
@@ -0,0 +1,21 @@
+package Package;
+
+public class Jugador {
+
+	private int Puntaje;
+
+	public void getPuntaje() {
+		// TODO - implement Jugador.getPuntaje
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Puntaje
+	 */
+	public void setPuntaje(int Puntaje) {
+		// TODO - implement Jugador.setPuntaje
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ src/main/java/Package2/VamoAjuga.java
@@ -0,0 +1,10 @@
+package Package2;
+
+public class VamoAjuga {
+
+	public void Juego() {
+		// TODO - implement VamoAjuga.Juego
+		throw new UnsupportedOperationException();
+	}
+
+}
